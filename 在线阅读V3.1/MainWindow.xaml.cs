1a8f174cbc7268094a2d30116ed4e2e779a7020b
zx648383079 <648383079@qq.com>
2015-07-05 17:11:25 +0800

    上传V3.1

---
+++ 在线阅读V3.1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using 在线阅读.Class;
+using 在线阅读.View;
+
+namespace 在线阅读
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private BookPage _bookPage;
+
+        private ReadPage _readPage;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _bookPage = new BookPage();
+            MainFrame.Navigate(_bookPage);
+        }
+
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem item = (MenuItem) sender;
+            switch (item.Header.ToString())
+            {
+                case "选项":
+                    SetWindow set = new SetWindow();
+                    if (set.ShowDialog()==true)
+                    {
+                        
+                    }
+                    break;
+                    
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            UserMethod method = new UserMethod();
+            method.SavaBook(Model.BookModels);
+        }
+    }
+}
---
+++ 在线阅读V3.1/View/RegexPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using 在线阅读.Class;
+using 在线阅读.Models;
+
+namespace 在线阅读.View
+{
+    /// <summary>
+    /// Interaction logic for RegexPage.xaml
+    /// </summary>
+    public partial class RegexPage : Page
+    {
+        public RegexPage()
+        {
+            InitializeComponent();
+        }
+
+        public SetWindow Set { get; set; }
+        private int _index;
+        
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MenuItem menu = (MenuItem) sender;
+            switch (menu.Header.ToString())
+            {
+                case "删除":
+                    _index = RegexListView.SelectedIndex;
+                    int count = Set.BookRegexs.Count;
+                    if (_index>=0 && _index<count)
+                    {
+                        Set.BookRegexs.RemoveAt(_index);
+                        Refresh();
+                    }
+                    break;
+                case "添加":
+                    ScrollGrid.Opacity = 1;
+                    WebTb.Text = "";
+                    ChapterTb.Text = "";
+                    ContentTb.Text = "";
+                    ReplaceTb.Text = "";
+                    _index = -1;
+                    break;
+            }
+        }
+
+        private void Refresh()
+        {
+            RegexListView.ItemsSource = null;
+            RegexListView.ItemsSource = Set.BookRegexs;
+        }
+
+        private void RegexListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            _index = RegexListView.SelectedIndex;
+            int count = Set.BookRegexs.Count;
+            if (_index < 0 || _index >= count) return;
+            ScrollGrid.Opacity = 1;
+            BookRegex regex = Set.BookRegexs[_index];
+            WebTb.Text = regex.Web;
+            ChapterTb.Text = regex.Chapter;
+            ContentTb.Text = regex.Content;
+            ReplaceTb.Text = regex.Replace;
+        }
+
+        private void SavaBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UserMethod method = new UserMethod();
+            string web = method.GetWeb(WebTb.Text);
+            BookRegex regex = new BookRegex(web,  ChapterTb.Text, ContentTb.Text,ReplaceTb.Text);
+            if (_index>=0)
+            {
+                
+                Set.BookRegexs[_index] =regex;
+            }
+            else
+            {
+                Set.BookRegexs.Add(regex);
+            }
+            Refresh();
+            ScrollGrid.Opacity = 0;
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Refresh();
+        }
+    }
+}
